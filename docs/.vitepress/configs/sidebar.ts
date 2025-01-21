6868bb7e282a1a64e4dc4e745bb4f52b1cdb007e
fullstackren <liferzy@163.com>
2025-01-21 16:02:30 +0800

    feat:langchain

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,22 +1,5 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const sidebar: DefaultTheme.Config['sidebar'] = {
-  '/dev-tools/': [
-    {
-      text: '快捷技巧',
-      items: [
-        { text: '快捷技巧', link: '/quick-tips' },
-        { text: '标签页分组', link: '/quick-tips/tab-group' },
-      ]
-    }
-  ],
-  '/quick-tips/': [
-    {
-      text: '快捷技巧',
-      items: [
-        { text: '快捷技巧', link: '/quick-tips' },
-        { text: '标签页分组', link: '/quick-tips/tab-group' },
-      ]
-    }
-  ]
+  
 }
